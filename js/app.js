5270d9182f0d9d2e247afd1a77109993c4b6444f
raphael <moudens.raphael@orange.fr>
2017-02-08 11:24:06 +0100

    modif css,ajout fonctions click clear et ajoute, message de bienvenu. Aymeric

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-alert("Welcome!!");
-
-
-$(document).ready(function(){
-	$("#clear").click(function({
-				// console.log('hello');
-	}));
-
+$( document ).ready(function(){
+	alert("la bienvenue");
+	console.log("du texte dans la console Js du navigateur");
+	$(".clear").click(function(){
+		console.log("efface");
+	});
+	$(".ajout").click(function(){
+		$("#todoList").append($("<li>" + + "</li>"));
+		console.log("ajoute");
+	});
 });
-
